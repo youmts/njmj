e349504588b625ec05d76453c20b88808628c76f
You Matsumoto <anata0814@gmail.com>
2018-01-02 22:34:49 +0900

    create nav-bar to home/index

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 feature 'Home' do
   scenario 'visit root' do
     visit root_path
-    expect(page).to have_content 'Hello world!'
+    expect(page).to have_content 'ゲームを開始'
   end
 end
