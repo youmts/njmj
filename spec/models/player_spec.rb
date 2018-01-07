868937e12ce412b7523c38d07e9c844582d58dde
You Matsumoto <anata0814@gmail.com>
2018-01-08 07:45:39 +0900

    fix rspec description

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,54 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe Player, type: :model do
-  context 'in room' do
-    let(:room) { Room.new }
-    it 'belongs to the room after create' do
-      player = Player.create(room: room)
-      expect(player.room).to eq room
-    end
-  end
-
-  context 'not in room' do
-    context 'when there are no available rooms' do
-      before :each do
-        create(:room, status: :in_game)
-        create(:room, status: :after_game)
-      end
-
-      it 'belongs to new room after create' do
-        player = Player.create
-        expect(player.room).not_to be_nil
-      end
-
-      it 'creates new room' do
-        expect {
-          player = Player.create
-        }.to change(Room, :count).by 1
+  describe '#create' do
+    context '既に部屋に所属しているとき' do
+      let(:room) { Room.new }
+      it 'その部屋に所属したまであること' do
+        player = Player.create(room: room)
+        expect(player.room).to eq room
       end
     end
 
-    context 'when there are any available rooms 'do
-      before :each do
-        @available_room = create(:room, status: :before_game)
-        create(:room, status: :in_game)
-        create(:room, status: :after_game)
-      end
+    context '部屋に所属していないとき' do
+      context '所属可能な部屋が存在しないとき' do
+        before :each do
+          create(:room, status: :in_game)
+          create(:room, status: :after_game)
+        end
 
-      it 'belongs to new room after create' do
-        player = Player.create
-        expect(player.room).not_to be_nil
-      end
-
-      it 'not creates new room' do
-        expect {
+        it '何らかの部屋に所属していること' do
           player = Player.create
-        }.to change(Room, :count).by 0
+          expect(player.room).not_to be_nil
+        end
+
+        it '部屋が作成されていること' do
+          expect {
+            player = Player.create
+          }.to change(Room, :count).by 1
+        end
       end
 
-      it 'belonged room is the available room' do
-        player = Player.create
-        expect(player.room).to eq @available_room
+      context '所属可能な部屋が存在するとき' do
+        before :each do
+          @available_room = create(:room, status: :before_game)
+          create(:room, status: :in_game)
+          create(:room, status: :after_game)
+        end
+
+        it 'その部屋に所属していること' do
+          player = Player.create
+          expect(player.room).to eq @available_room
+        end
+
+        it '新たな部屋は作成しないこと' do
+          expect {
+            player = Player.create
+          }.to change(Room, :count).by 0
+        end
       end
     end
   end
--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
-  it 'has some players' do
+  it '複数のプレイヤーを持てること' do
     room = build(:room)
     expect(room).to be_valid
     expect(room.players.count).to eq 1
